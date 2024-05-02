b3ec9175f00c7c60e9ccfbf38ba27ade5a34de43
Marta Rodrigo <martarodrigo.55@gmail.com>
2024-05-02 17:32:13 +0200

    desarrollo nav y algunas partes de footer e index

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -11,3 +11,19 @@
  */
 
 import * as bootstrap from 'bootstrap';
+
+
+/****MENÚ HAMBURGUESA******/
+document.addEventListener("DOMContentLoaded", function() {
+    const nav = document.querySelector("#nav");
+    const abrir = document.querySelector("#abrir");
+    const cerrar = document.querySelector("#cerrar");
+
+    abrir.addEventListener("click", () => {
+        nav.classList.add("main-nav--visible"); 
+    });
+
+    cerrar.addEventListener("click", () => {
+        nav.classList.remove("main-nav--visible"); 
+    });
+});
