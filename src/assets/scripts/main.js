7d0b8c45a019c5d6b1d4c48cf5a8ea1c1c61a410
Marta Rodrigo <martarodrigo.55@gmail.com>
2024-05-04 16:19:57 +0200

    avanzo con peticiones de index y chefs

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -13,6 +13,7 @@
 import * as bootstrap from 'bootstrap';
 
 
+
 /****MENÚ HAMBURGUESA******/
 document.addEventListener("DOMContentLoaded", function() {
     const nav = document.querySelector("#nav");
@@ -27,3 +28,40 @@ document.addEventListener("DOMContentLoaded", function() {
         nav.classList.remove("main-nav--visible"); 
     });
 });
+
+/***CURRENT PAGE***/
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtenemos la URL de la página actual
+    const currentUrl = window.location.href;
+    
+    // Obtenemos todas las etiquetas <a> dentro de la lista de navegación
+    const navLinks = document.querySelectorAll(".main-nav__link");
+
+    // Iteramos sobre cada enlace
+    navLinks.forEach(function(link) {
+        // Verificamos si la URL del enlace coincide con la URL de la página actual
+        if (link.href === currentUrl) {
+            // Agregamos la clase "current-page" a la etiqueta <li> padre del enlace
+            link.parentNode.classList.add("current-page");
+        }
+    });
+});
+
+import Swiper from 'swiper';
+// Importar los estilos de Swiper y los módulos necesarios
+import 'swiper/swiper-bundle.css';
+
+/***SWIPER***/
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+     
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
